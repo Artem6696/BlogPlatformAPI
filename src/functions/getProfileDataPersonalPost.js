6fb5335bfdd8132df242aabefdbed9e3d71fcee5
Artem <hanya66693@gmail.com>
2023-06-26 16:24:29 +0500

    Update entire design, add user posts functionality, change user avatar, and update user data

---
+++ src/functions/getProfileDataPersonalPost.js
@@ -0,0 +1,9 @@
+const getProfileDataPersonalPost = async () => {
+    const userId = localStorage.getItem("userId")
+
+    const response = await fetch(`http://cepbep.ddns.net:2500/api/postDB/posts/getUserPosts/${userId}`)
+
+    return await response.json()
+}
+
+export default getProfileDataPersonalPost
